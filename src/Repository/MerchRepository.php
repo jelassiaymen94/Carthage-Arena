a7f009e063d44c186bf65d41e48f71ece842522c
Bilel <avebilel10@gmail.com>
2026-02-24 15:25:22 +0100

    Update project

--- src/Repository/MerchRepository.php
+++ src/Repository/MerchRepository.php
@@ -1,18 +1,26 @@
 <?php
-
 namespace App\Repository;
 
 use App\Entity\Merch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Merch>
- */
 class MerchRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Merch::class);
+    }
+
+    /**
+     * Search merch by name or type
+     */
+    public function search(string $query): array
+    {
+        return $this->createQueryBuilder('m')
+            ->where('m.name LIKE :q OR m.type LIKE :q')
+            ->setParameter('q', '%'.$query.'%')
+            ->getQuery()
+            ->getResult();
     }
 }
